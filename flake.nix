3c4ba81935aa00e1e3c79e564d6f001d4a6b04dd
Joshua Chapman <joshua.chapman@secomind.com>
2023-03-29 16:39:04 +0200

    feat(nix): add flake.nix and .envrc
    Signed-off-by: Joshua Chapman <joshua.chapman@secomind.com>

---
+++ flake.nix
@@ -0,0 +1,66 @@
+# This file is part of Astarte.
+# Copyright 2023 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+{
+  description = "A central service that runs on (Linux) devices for collecting and delivering messages from N apps using 1 MQTT connection to Astarte.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    naersk.url = "github:nix-community/naersk";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = pkgs.rust-bin.stable."1.59.0".default;
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+      in
+      with pkgs;
+      {
+        packages.default = naersk'.buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            protobuf
+          ];
+          buildInputs = with pkgs; [
+            openssl
+          ];
+          # This is required to disable vendoring, we do not need vendoring since we are using nix.
+          OPENSSL_NO_VENDOR = 1;
+        };
+        devShells.default = mkShell {
+          packages = [
+            toolchain
+            openssl
+            pkg-config
+            protobuf
+          ];
+        };
+      }
+    );
+}
