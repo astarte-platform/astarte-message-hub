0cab3e61535cf59737dcbc74b27fc2f2fefe91ac
Joshua Chapman <joshua.chapman@secomind.com>
2023-03-29 17:58:47 +0200

    feat(proto): improve ergonomics of AstarteMessage
    Add methods to AstarteMessage to make it easier to handle the
    different payloads. This will return an Option of the inner type that
    can be mapped without doing pattern matching.
    
    Signed-off-by: Joshua Chapman <joshua.chapman@secomind.com>

--- src/lib.rs
+++ src/lib.rs
@@ -30,5 +30,111 @@ mod types;
 
 #[allow(clippy::all)]
 pub mod proto_message_hub {
+
     tonic::include_proto!("astarteplatform.msghub");
+
+    impl AstarteMessage {
+        pub fn take_data(self) -> Option<AstarteDataType> {
+            use self::astarte_message::Payload;
+
+            match self.payload {
+                Some(Payload::AstarteData(data)) => Some(data),
+                _ => None,
+            }
+        }
+
+        pub fn take_unset(self) -> Option<AstarteUnset> {
+            use self::astarte_message::Payload;
+
+            match self.payload {
+                Some(Payload::AstarteUnset(unset)) => Some(unset),
+                _ => None,
+            }
+        }
+
+        pub fn data(&self) -> Option<&AstarteDataType> {
+            use self::astarte_message::Payload;
+
+            match self.payload {
+                Some(Payload::AstarteData(ref data)) => Some(data),
+                _ => None,
+            }
+        }
+
+        pub fn unset(&self) -> Option<&AstarteUnset> {
+            use self::astarte_message::Payload;
+
+            match self.payload {
+                Some(Payload::AstarteUnset(ref unset)) => Some(unset),
+                _ => None,
+            }
+        }
+
+        pub fn data_mut(&mut self) -> Option<&mut AstarteDataType> {
+            use self::astarte_message::Payload;
+
+            match self.payload {
+                Some(Payload::AstarteData(ref mut data)) => Some(data),
+                _ => None,
+            }
+        }
+
+        pub fn unset_mut(&mut self) -> Option<&mut AstarteUnset> {
+            use self::astarte_message::Payload;
+
+            match self.payload {
+                Some(Payload::AstarteUnset(ref mut unset)) => Some(unset),
+                _ => None,
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::proto_message_hub::{
+        astarte_message::Payload, AstarteDataType, AstarteMessage, AstarteUnset,
+    };
+
+    #[test]
+    fn test_astarte_message_data() {
+        let data = AstarteDataType::default();
+        let mut message = AstarteMessage {
+            payload: Some(Payload::AstarteData(data.clone())),
+            ..Default::default()
+        };
+
+        // This test also the ergonomics of the methods
+        assert!(message.data().is_some());
+        assert!(message.data_mut().is_some());
+        assert!(message.clone().take_data().is_some());
+        assert!(message.unset().is_none());
+        assert!(message.unset_mut().is_none());
+        assert!(message.clone().take_unset().is_none());
+
+        let res = message.take_data();
+
+        assert_eq!(res, Some(data));
+    }
+
+    #[test]
+    fn test_astarte_message_unset() {
+        let unset = AstarteUnset::default();
+        let mut message = AstarteMessage {
+            payload: Some(Payload::AstarteUnset(unset.clone())),
+            ..Default::default()
+        };
+
+        // This test also the ergonomics of the methods
+        assert!(message.data().is_none());
+        assert!(message.data_mut().is_none());
+        assert!(message.clone().take_data().is_none());
+        assert!(message.unset().is_some());
+        assert!(message.unset_mut().is_some());
+        assert!(message.clone().take_unset().is_some());
+
+        let res = message.take_unset();
+
+        assert_eq!(res, Some(unset));
+    }
 }
