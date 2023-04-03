bd680d976ccaa934ab45af20b0b22f315062d3fe
Simone Orru <simone.orru@secomind.com>
2023-04-03 14:30:48 +0200

    Add server to main
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

--- src/error.rs
+++ src/error.rs
@@ -49,4 +49,7 @@ pub enum AstarteMessageHubError {
 
     #[error("configuration file error")]
     ConfigFileError(#[from] toml::de::Error),
+
+    #[error("Protobuf transport error")]
+    TransportError(#[from] tonic::transport::Error),
 }
