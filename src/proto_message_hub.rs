d0327d495d741ae426dc33c117f891849c90e524
Joshua Chapman <joshua.chapman@secomind.com>
2023-04-19 10:42:22 +0200

    refactor(proto): improve the helper functions
    Add the `#[must_use]` attribute to the helper functions that return an
    Option. This will help the user to remember to handle the case where the
    function returns None. Also, add the specific variant to the match
    statement, instead of using the _ wildcard.
    
    Signed-off-by: Joshua Chapman <joshua.chapman@secomind.com>

--- src/proto_message_hub.rs
+++ src/proto_message_hub.rs
@@ -1,4 +1,5 @@
 // This file is part of Astarte.
+//
 // Copyright 2023 SECO Mind Srl
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,81 +26,93 @@ tonic::include_proto!("astarteplatform.msghub");
 
 impl Payload {
     /// Takes the [Payload::AstarteData] variant value out of the enum.
+    #[must_use]
     pub fn take_data(self) -> Option<AstarteDataType> {
         match self {
             Payload::AstarteData(data) => Some(data),
-            _ => None,
+            Payload::AstarteUnset(_) => None,
         }
     }
 
     /// Returns a reference to the [Payload::AstarteData] variant value.
+    #[must_use]
     pub fn data(&self) -> Option<&AstarteDataType> {
         match self {
             Payload::AstarteData(ref data) => Some(data),
-            _ => None,
+            Payload::AstarteUnset(_) => None,
         }
     }
 
     /// Returns a mutable reference to the [Payload::AstarteData] variant value.
+    #[must_use]
     pub fn data_mut(&mut self) -> Option<&mut AstarteDataType> {
         match self {
             Payload::AstarteData(ref mut data) => Some(data),
-            _ => None,
+            Payload::AstarteUnset(_) => None,
         }
     }
 
     /// Takes the [Payload::AstarteUnset] variant value out of the enum.
+    #[must_use]
     pub fn take_unset(self) -> Option<AstarteUnset> {
         match self {
             Payload::AstarteUnset(unset) => Some(unset),
-            _ => None,
+            Payload::AstarteData(_) => None,
         }
     }
 
     /// Returns a reference to the [Payload::AstarteUnset] variant value.
+    #[must_use]
     pub fn unset(&self) -> Option<&AstarteUnset> {
         match self {
             Payload::AstarteUnset(ref unset) => Some(unset),
-            _ => None,
+            Payload::AstarteData(_) => None,
         }
     }
 
     /// Returns a mutable reference to the [Payload::AstarteUnset] variant value.
+    #[must_use]
     pub fn unset_mut(&mut self) -> Option<&mut AstarteUnset> {
         match self {
             Payload::AstarteUnset(ref mut unset) => Some(unset),
-            _ => None,
+            Payload::AstarteData(_) => None,
         }
     }
 }
 
 impl AstarteMessage {
     /// Takes the [Payload::AstarteData] variant value out of the [payload](AstarteMessage::payload) enum.
+    #[must_use]
     pub fn take_data(self) -> Option<AstarteDataType> {
         self.payload.and_then(Payload::take_data)
     }
 
     /// Returns a reference to the [Payload::AstarteData] variant value of the [payload](AstarteMessage::payload) enum.
+    #[must_use]
     pub fn data(&self) -> Option<&AstarteDataType> {
         self.payload.as_ref().and_then(Payload::data)
     }
 
     /// Returns a mutable reference to the [Payload::AstarteData] variant value of the [payload](AstarteMessage::payload) enum.
+    #[must_use]
     pub fn data_mut(&mut self) -> Option<&mut AstarteDataType> {
         self.payload.as_mut().and_then(Payload::data_mut)
     }
 
     /// Takes the [Payload::AstarteUnset] variant value out of the [payload](AstarteMessage::payload) enum.
+    #[must_use]
     pub fn take_unset(self) -> Option<AstarteUnset> {
         self.payload.and_then(Payload::take_unset)
     }
 
     /// Returns a reference to the [Payload::AstarteUnset] variant value of the [payload](AstarteMessage::payload) enum.
+    #[must_use]
     pub fn unset(&self) -> Option<&AstarteUnset> {
         self.payload.as_ref().and_then(Payload::unset)
     }
 
     /// Returns a mutable reference to the [Payload::AstarteUnset] variant value of the [payload](AstarteMessage::payload) enum.
+    #[must_use]
     pub fn unset_mut(&mut self) -> Option<&mut AstarteUnset> {
         self.payload.as_mut().and_then(Payload::unset_mut)
     }
@@ -107,50 +120,56 @@ impl AstarteMessage {
 
 impl Data {
     /// Takes the [Data::AstarteIndividual] variant value out of the enum.
+    #[must_use]
     pub fn take_individual(self) -> Option<AstarteDataTypeIndividual> {
         match self {
             Data::AstarteIndividual(individual) => Some(individual),
-            _ => None,
+            Data::AstarteObject(_) => None,
         }
     }
 
     /// Returns a reference to the [Data::AstarteIndividual] variant value.
+    #[must_use]
     pub fn individual(&self) -> Option<&AstarteDataTypeIndividual> {
         match self {
             Data::AstarteIndividual(ref individual) => Some(individual),
-            _ => None,
+            Data::AstarteObject(_) => None,
         }
     }
 
     /// Returns a mutable reference to the [Data::AstarteIndividual] variant value.
+    #[must_use]
     pub fn individual_mut(&mut self) -> Option<&mut AstarteDataTypeIndividual> {
         match self {
             Data::AstarteIndividual(ref mut individual) => Some(individual),
-            _ => None,
+            Data::AstarteObject(_) => None,
         }
     }
 
     /// Takes the [Data::AstarteObject] variant value out of the enum.
+    #[must_use]
     pub fn take_object(self) -> Option<AstarteDataTypeObject> {
         match self {
             Data::AstarteObject(object) => Some(object),
-            _ => None,
+            Data::AstarteIndividual(_) => None,
         }
     }
 
     /// Returns a reference to the [Data::AstarteObject] variant value.
+    #[must_use]
     pub fn object(&self) -> Option<&AstarteDataTypeObject> {
         match self {
             Data::AstarteObject(ref object) => Some(object),
-            _ => None,
+            Data::AstarteIndividual(_) => None,
         }
     }
 
     /// Returns a mutable reference to the [Data::AstarteObject] variant value.
+    #[must_use]
     pub fn object_mut(&mut self) -> Option<&mut AstarteDataTypeObject> {
         match self {
             Data::AstarteObject(ref mut object) => Some(object),
-            _ => None,
+            Data::AstarteIndividual(_) => None,
         }
     }
 }
