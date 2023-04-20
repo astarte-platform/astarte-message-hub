9c04ff344ab92e76742bdd2003683de21b183347
Joshua Chapman <joshua.chapman@secomind.com>
2023-04-20 12:04:22 +0200

    feat(examples): add a count option to the client example
    The client example will now stop after sending COUNT messages, if
    specified. This is useful to test the message hub with a limited
    number of messages will close correctly.
    
    Signed-off-by: Joshua Chapman <joshua.chapman@secomind.com>

--- examples/client/main.rs
+++ examples/client/main.rs
@@ -17,6 +17,9 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+//! Astarte Message Hub client example, will send the uptime every 3 seconds to Astarte.
+
 use std::time;
 
 use clap::Parser;
@@ -25,23 +28,24 @@ use astarte_message_hub::proto_message_hub::astarte_message::Payload;
 use astarte_message_hub::proto_message_hub::message_hub_client::MessageHubClient;
 use astarte_message_hub::proto_message_hub::AstarteMessage;
 use astarte_message_hub::proto_message_hub::Node;
-use log::error;
+use log::info;
 
 /// Create a ProtoBuf client for the Astarte message hub.
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(version, about)]
 struct Cli {
     /// UUID to be used when registering the client as an Astarte message hub node.
-    #[clap(value_parser)]
     uuid: String,
+
+    /// Stop after sending COUNT messages.
+    #[clap(short, long)]
+    count: Option<u64>,
 }
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
     let args = Cli::parse();
-
-    let now = time::SystemTime::now();
 
     let mut client = MessageHubClient::connect("http://[::1]:50051")
         .await
@@ -58,26 +62,32 @@ async fn main() {
 
     let node = Node::new(&args.uuid, &interface_jsons);
 
-    let mut stream = client
-        .attach(tonic::Request::new(node))
-        .await
-        .unwrap()
-        .into_inner();
+    let mut stream = client.attach(node.clone()).await.unwrap().into_inner();
 
     // Start a separate task to handle incoming data
     let reply_handle = tokio::spawn(async move {
+        info!("Waiting for messages from the message hub.");
+
         while let Some(astarte_message) = stream.message().await.unwrap() {
             println!("Received AstarteMessage = {:?}", astarte_message);
         }
+
+        info!("Done receiving messages, closing the connection.");
     });
 
     // Start a separate task to publish data
     let send_handle = tokio::spawn(async move {
+        let now = time::SystemTime::now();
+        let mut count = 0;
         // Consistent interval of 3 seconds
         let mut interval = tokio::time::interval(std::time::Duration::from_secs(3));
 
-        loop {
+        while args.count.is_none() || Some(count) < args.count {
+            // Wait a little
+            interval.tick().await;
+
             println!("Publishing the uptime through the message hub.");
+
             let elapsed = now.elapsed().unwrap().as_secs();
 
             let elapsed_str = format!("Uptime for node {}: {}", args.uuid, elapsed);
@@ -90,16 +100,18 @@ async fn main() {
             };
             client.send(msg).await.unwrap();
 
-            // Wait a little
-            interval.tick().await;
+            count += 1;
         }
+
+        info!("Done sending messages, closing the connection.");
+        client.detach(node).await.expect("Detach failed");
     });
 
     let res = tokio::join!(reply_handle, send_handle);
 
     match res {
         (Ok(_), Ok(_)) => (),
-        (Err(e), Ok(_)) | (Ok(_), Err(e)) => error!("Error: {}", e),
-        (Err(e1), Err(e2)) => error!("Error:\n\t{}\n\t{}", e1, e2),
+        (Err(e), Ok(_)) | (Ok(_), Err(e)) => panic!("Error: {}", e),
+        (Err(e1), Err(e2)) => panic!("Error:\n\t{}\n\t{}", e1, e2),
     }
 }
